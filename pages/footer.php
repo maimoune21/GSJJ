387eda458a951d180a5400df3243aa02cb8275f2
Maimoune Mohamed <111010895+maimoune21@users.noreply.github.com>
2024-10-27 04:06:58 +0000

    Update footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -1,4 +1,5 @@
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="./icons/css/all.min.css">
     <link rel="stylesheet" href="../css/styles/footer.css">
     <link rel="stylesheet" href="../css/queries/footer.css">
