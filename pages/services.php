54cf773afbbc5d48a5a1f83de8a82a5be70f6d1b
Maimoune Mohamed <111010895+maimoune21@users.noreply.github.com>
2024-10-28 03:06:40 +0000

    Update services.php

--- pages/services.php
+++ pages/services.php
@@ -22,7 +22,44 @@
 </head>
 
 <body>
-    <?php include_once "./nav.php" ?>
+    <header>
+    <img src="../data/pictures/GSJJ-logo.png" class="main-img" alt="">
+    <nav id="home">
+        <div class="menu-nav-container">
+            <ul class="menu-nav">
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="../pages/hohaires.php">Hohaires</a></li>
+                <li><a href="../pages/services.php">Services</a></li>
+                <li class="c-1"><a href="../pages/philosophie.php">Philosophie</a></li>
+                <li class="c-1"><a href="../pages/activites.php">Activités parascolaires</a></li>
+                <li class="c-1"><a href="../pages/temoignages.php">Témoignages</a></li>
+                <li><a href="../pages/calendrier.php">Calendrier de vacance</a></li>
+                <li>
+                    <div class="language-dropdown">
+                        <button id="english-button" aria-haspopup="true" aria-expanded="false">
+                            <img src="../data/pictures/english.png" alt="English flag" style="height: 1.3em;">
+                            <span>ENGLISH</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="english-button" role="menu">
+                            <li role="menuitem">
+                                <a href="#fr">
+                                    <img src="../data/pictures/french.png" alt="French flag" style="height: 1.3em;">
+                                    <span>Français</span>
+                                </a>
+                            </li>
+                            <li role="menuitem">
+                                <a href="#ar">
+                                    <img src="../data/pictures/arabic.png" alt="Arabic flag" style="height: 1.3em;">
+                                    <span>العربية</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
     <section class="header">
         <h1>Services</h1>
     </section>
